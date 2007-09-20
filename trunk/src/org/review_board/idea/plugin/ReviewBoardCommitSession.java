3925b98d33b1519849b4a04f092c9f62af924ad6
plumpy <plumpy@b5961e6b-5f39-0410-ace1-d5e7866dc15a>
2007-09-20 11:43:30 +0000

    Switch to using the IDEA built-in (but undocumented) PatchBuilder and UnifiedDiffWriter.

--- trunk/src/org/review_board/idea/plugin/ReviewBoardCommitSession.java
+++ trunk/src/org/review_board/idea/plugin/ReviewBoardCommitSession.java
@@ -1,19 +1,24 @@
 package org.review_board.idea.plugin;
 
-import com.intellij.openapi.vcs.changes.CommitSession;
-import com.intellij.openapi.vcs.changes.Change;
-import com.intellij.openapi.vcs.changes.ContentRevision;
-import com.intellij.openapi.vcs.VcsException;
+import com.intellij.openapi.diff.impl.patch.FilePatch;
+import com.intellij.openapi.diff.impl.patch.PatchBuilder;
+import com.intellij.openapi.diff.impl.patch.UnifiedDiffWriter;
 import com.intellij.openapi.project.Project;
-import org.jetbrains.annotations.Nullable;
-import javax.swing.JComponent;
+import com.intellij.openapi.vcs.changes.Change;
+import com.intellij.openapi.vcs.changes.CommitSession;
+import java.io.StringWriter;
 import java.util.Collection;
+import java.util.List;
+import javax.swing.JComponent;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class ReviewBoardCommitSession implements CommitSession
 {
+    @NotNull
     private Project m_project;
 
-    public ReviewBoardCommitSession( final Project project )
+    public ReviewBoardCommitSession( @NotNull final Project project )
     {
         m_project = project;
     }
@@ -32,28 +37,33 @@ public class ReviewBoardCommitSession implements CommitSession
 
     public void execute( final Collection<Change> changes, final String commitMessage )
     {
-        for( final Change change: changes )
+        try
         {
-            final ContentRevision beforeContent = change.getBeforeRevision();
-            final ContentRevision afterContent = change.getAfterRevision();
-            final String[] before, after;
-            try
-            {
-                before = beforeContent.getContent().split( "\n" );
-                after = afterContent.getContent().split( "\n" );
-            }
-            catch ( VcsException e )
-            {
-                e.printStackTrace();
-                return;
-            }
-            final Diff diff = new Diff( before, after );
-            final Diff.change script = diff.diff_2( false ); 
-            final DiffPrint.UnifiedPrint differ =
-                new DiffPrint.UnifiedPrint( before, after );
-            differ.print_header( beforeContent.getFile().getPath(),
-                afterContent.getFile().getPath() );
-            differ.print_script( script );
+            /* Here's some IDEA code that isn't part of openapi, but which I was
+             * encouraged to use:
+             *   http://www.jetbrains.net/jira/browse/IDEADEV-21340
+             *
+             * The second argument is the path which will be stripped from the diff
+             * filenames.  So if you are diffing two versions of
+             * "/home/plumpy/hello/Foo.java" and the second argument is "/home/plumpy",
+             * then the filename in the diff will be "hello/Foo.java".
+             *
+             * The third argument tells it to ignore the fact that a file moved, treating
+             * it as a normal modification.  If this is false, the patch contains a
+             * deletion of the old file and an addition of the new file.
+             *
+             * The fourth argument tells it whether or not to reverse the order of the
+             * patch.
+             */
+            List<FilePatch> patches = PatchBuilder
+                .buildPatch( changes, m_project.getBaseDir().getPath(), true, false );
+            StringWriter writer = new StringWriter( 2048 );
+            UnifiedDiffWriter.write( patches, writer );
+            System.out.print( writer.toString() );
+        }
+        catch ( Exception e )
+        {
+            e.printStackTrace();
         }
     }
 
